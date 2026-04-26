5a7dc9e96899cea5d1397c559383fbc15e6c767c
Maurizio De Santis <desantis.maurizio@gmail.com>
2026-04-26 21:57:08 +0200

    test: add Vite SSR sample rendering integration test
    Adds test_render_vite_ssr_sample which:
    1. Initializes the runtime with the sample bundle
    2. Calls SSR::Deno.render with component_data, props, and url
    3. Asserts the output contains expected HTML structure
    
    Also adds test_native_version to verify the native_version method.

--- test/ssr/test_deno.rb
+++ test/ssr/test_deno.rb
@@ -1,11 +1,38 @@
 # frozen_string_literal: true
 
+require 'json'
 require 'test_helper'
 
 module SSR
   class TestDeno < Minitest::Test
     def test_that_it_has_a_version_number
       refute_nil ::SSR::Deno::VERSION
+    end
+
+    def test_native_version
+      assert_match(/\A\d+\.\d+\.\d+\z/, ::SSR::Deno.native_version)
+    end
+
+    def test_render_vite_ssr_sample
+      bundle_path = File.expand_path('../../samples/vite-ssr-app/dist/server/entry-server.js', __dir__)
+
+      assert_path_exists bundle_path, "Bundle not found at #{bundle_path}"
+
+      result = ::SSR::Deno.init_runtime(bundle_path)
+
+      assert_equal 'Runtime initialized successfully', result
+
+      args = JSON.generate(
+        component_data: { message: 'Hello World!' },
+        props: {},
+        url: '/'
+      )
+      html = ::SSR::Deno.render(args)
+
+      assert_match(%r{<html>.*</html>}m, html)
+      assert_includes html, 'Hello'
+      assert_includes html, 'World'
+      assert_includes html, '<div id="root">'
     end
   end
 end
